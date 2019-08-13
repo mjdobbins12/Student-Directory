5671deb4ad80e7ac68cf394ed97e98ebd3961d9a
mjdobbins12@gmail.com <mjdobbins12@gmail.com>
2019-08-13 14:25:46 +0100

    read and write file methods

--- directory.rb
+++ directory.rb
@@ -1,5 +1,14 @@
 @students = []
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
